a9ce6cf8612500465c808d147c2f223addd7ad3c
Abraham <abukip344@gmail.com>
2024-03-19 23:24:11 -0400

    maths

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+//random integer greater than 0
+const random = Math.ceil(Math.random() * 10);
+
+//calculating the remainder
+let num3 = 44;
+let num4 = 10;
+const mod = num3 % num4;
+
+//highest number in a set
+const max = Math.max(15, 10, 18, 11, 20);
+console.log(max);
+
